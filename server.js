5aa4ec2739626ae2758f88045484f14c99979426
emir.palma <emirpalm@outlook.com>
2022-09-22 14:09:24 -0500

    package.json

--- server.js
+++ server.js
@@ -3,12 +3,11 @@ const path = require('path');
 
 const app = express();
 
-
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/dist/micaela'));
 
 app.get('/*', function(req,res) {
- 
-res.sendFile(path.join(__dirname + '/index.html'));
-});
+
+res.sendFile(path.join(__dirname + '/dist/micaela/index.html'));}
+);
 
 app.listen(process.env.PORT || 8080);
