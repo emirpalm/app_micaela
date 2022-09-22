a033663443bc5c0b000756998911b34ead8dd9cc
emir.palma <emirpalm@outlook.com>
2022-09-22 13:48:44 -0500

    package.json

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/dist/micaela'));
 
 app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname+'/dist/micaela/index.html'));
+res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
